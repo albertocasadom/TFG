d0d8e0b6714be034aa9f3be10c10f6a5575e0e9c
Alberto <albertocasadom@gmail.com>
2019-02-17 18:37:36 +0100

    Django framework added

---
+++ trainingweb/applications/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+import os
+import json
+def index(request):
+	user = "medinaca"
+	template = loader.get_template('applications/index.html')
+	with open('/home/hacklberto/Telematics Degree/TFG/TFG/data.json','r') as datafile:
+		data = json.load(datafile)
+	context = { 'data':data}
+	return HttpResponse(template.render(context, request))
