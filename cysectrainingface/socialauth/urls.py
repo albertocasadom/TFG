ab8fe7ded3a0e60dbf4a4381b346e629ad5b7666
Alberto <albertocasadom@gmail.com>
2019-03-16 17:51:03 +0100

    Search in progress

--- cysectrainingface/socialauth/urls.py
+++ cysectrainingface/socialauth/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from socialauth.views import home, logout_view
+from socialauth.views import found, logout_view, search
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(template_name = 'socialauth/login.html')),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name = 'socialauth/login.html'), name = 'login'),
     url(r'^logout/$', logout_view, name = 'logout'),
+    url(r'^search/', search, name = 'search'),
     url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
-    url(r'^$', home, name='home'),
+    url(r'^found/', found, name='found'),
 ]
--- cysectrainingface/socialauth/views.py
+++ cysectrainingface/socialauth/views.py
@@ -2,27 +2,59 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth import logout
-
+import re
 import os
 import json
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def home(request):
-	template = loader.get_template('home.html')
+def found(request):
+	template = loader.get_template('found.html')
+	getfilter = request.POST.get('filter')
+	gettext = request.POST.get('text')
+	result = []
 	with open(os.path.join(BASE_DIR, '../data.json'),'r') as datafile:
 		data = json.load(datafile)
-	context = { 'data':data}
+	if getfilter != 'word':
+		for training in data['resources']:
+			if gettext in training[getfilter]:
+				result.append(training)
+			else:
+				continue
+	else:
+		ENISA_DIR = os.walk(os.path.join(BASE_DIR,'../EnisaFiles'))
+		repeated = 0
+		for dirs in ENISA_DIR:
+			file = os.path.join(dirs[0],'mainwordfile.txt')
+			if os.path.exists(file):
+				with open(file,'r') as wordsfile:
+					lines = wordsfile.readlines()
+					for line in lines:
+						words = line.split(' ')
+						for w in words:
+							repeated+=1
+							dirsname = dirs[0].split(BASE_DIR)
+							print(dirsname[1])
+
+		print(repeated)
+	context = {'data':result}
 	return HttpResponse(template.render(context, request))
 
 def logout_view(request):
-	template = loader.get_template('socialauth/logout.html')
-	context = {}
 	logout(request)
-	return HttpResponse(template.render(context,request))
+	return render(request,'socialauth/logout.html')
 
-def login(request):
-	template = loader.get_template('socialauth/login.html')
-	login(request)
-	context = {}
+def search(request):
+	template = loader.get_template('search.html')
+	with open(os.path.join(BASE_DIR, '../data.json'),'r') as datafile:
+		data = json.load(datafile)
+	searchfilter = []
+	for resources in data['resources']:
+		keys = list(resources.keys())
+		for key in keys:
+			if key in searchfilter:
+				continue
+			else:
+				searchfilter.append(key)
+	context = {'filter':searchfilter}
 	return HttpResponse(template.render(context,request))
